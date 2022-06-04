fe1d2e732a8e677ef6072d0fcf55fa6a99b0832a
Pablo Monardo <pablomonardo@gmail.com>
2022-06-04 15:47:43 -0300

    Update main.cpp
    I defined the operations that the program will show

--- DateProject/main.cpp
+++ DateProject/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include "Include/Date.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    Date date1(1,1,2020);
+    Date date2(15,12,2020);
+
+    int dif= date1.daysBetween(&date2);
+    int dif2 = date1 - &date2;
+
+    Date daysAfter = date1.addDays(60); 
 }
