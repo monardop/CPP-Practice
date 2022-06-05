13936e384b7c012ec23e086cb29acaf84ca340ac
Pablo Monardo <pablomonardo@gmail.com>
2022-06-05 10:58:41 -0300

    Issues were corrected
    Errors in the "Date.h" file are corrected and it is tested in main.

--- DateProject/include/Date.h
+++ DateProject/include/Date.h
@@ -2,12 +2,14 @@
 #define DATE_H
 #define BASE_YEAR 1601
 
+#include <iostream>
+using namespace std;
 class Date
 {
     private:
         int relDay;
-        static const int Date::amountDays[14];
-        static const int Date::amountLeapDays[14];
+        static const int amountDays[14];
+        static const int amountLeapDays[14];
         static int gregorianDay(int day, int month, int year);
         static bool leapYear(int year);
     public:
@@ -24,4 +26,5 @@ class Date
 };
 
 ostream& operator <<(ostream& out, const Date& d);
+
 #endif // DATE_H
--- DateProject/main.cpp
+++ DateProject/main.cpp
@@ -7,9 +7,10 @@ int main()
     Date date1(1,1,2020);
     Date date2(15,12,2020);
 
-    int dif= date1.daysBetween(&date2);
+    int dif= date1.daysBetween(date2);
 
     Date daysAfter = date1.addDays(60);
 
     std::cout << "The difference between 1/1/2020 and 15/12/2020 is " << dif << std::endl;
+    std::cout << "60 days after 1/1/2020 will be " << daysAfter << std::endl;
 }
